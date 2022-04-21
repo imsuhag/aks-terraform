39e361db0cfeb8ba1a7c2ffaa20c4511b6ca6219
Manjeet Suhag <manjeetsuhag@gmail.com>
2022-04-21 10:08:06 +0200

    define variables

---
+++ variables.tf
@@ -0,0 +1,26 @@
+# Azure Location
+variable "location" {
+  type = string
+  description = "Azure Region where all these resources will be provisioned"
+  default = "Central US"
+}
+
+# Azure Resource Group Name
+variable "resource_group_name" {
+  type = string
+  description = "This variable defines the Resource Group"
+  default = "terraform-aks"
+}
+
+# Azure AKS Environment Name
+variable "environment" {
+  type = string  
+  description = "This variable defines the Environment"  
+  default = "dev"
+}
+
+# SSH Public Key for Linux VMs
+variable "ssh_public_key" {
+  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+  description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
+}
