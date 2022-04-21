803d26963899dd9d83083b392e242e80a00f1cf7
Manjeet Suhag <manjeetsuhag@gmail.com>
2022-04-21 10:00:01 +0200

    inital commit of main file
    set the terraform version and dependent providers.

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "terraform-storage-rg"
+    storage_account_name  = "terraformstatexlrwdrzs"
+    container_name        = "tfstatefiles"
+    key                   = "dev.terraform.tfstate"
+  }  
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+resource "random_pet" "aksrandom" {
+
+}
